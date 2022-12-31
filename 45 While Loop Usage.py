3ee1ae9216cbf61b1a838a592d399ddfb8fc6ecd
sajibdigital <117761421+sajibdigital@users.noreply.github.com>
2022-12-31 21:26:25 +0600

    Module Five

---
+++ 45 While Loop Usage.py
@@ -0,0 +1,11 @@
+students = []
+while True:
+    name = input('Enter Students name: ')
+    if name == 'shesh':
+        print(students)
+        print('Total Studetns: ', len(students))
+        break
+    else:
+        students.append(name)
+
+
